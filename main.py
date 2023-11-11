224b60e6a97a3acdddae430666ce408878ea88f0
pistixy <71815251+pistixy@users.noreply.github.com>
2023-11-11 06:38:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
+# Importing necessary classes from other modules
 from cart import Cart
-from product import Product
 from products import products
 
 def main():
-    # Instantiate a new Cart object
+    # Create an instance of Cart, which will hold the selected products
     cart = Cart()
-    next =""
-    # Prompt the user to enter product code
-    while (next!="DONE"):
-        next=input("Enter one product code if you are done, enter 'DONE' (e.g., GR1,SR1,CF1): ").strip().upper()
-        # Check if the provided product code exists in the available products
+    
+    # Initialize a variable to control the while loop for product input
+    next = ""
+    
+    # Continue prompting the user for input until they type 'DONE'
+    while (next != "DONE"):
+        # Ask the user for a product code or to enter 'DONE' to finish shopping
+        next = input("Enter one product code or type 'DONE' to complete your purchase (e.g., GR1,SR1,CF1): ").strip().upper()
+        
+        # Check if the input is a product code that exists in our products list
         if next in products:
+            # Add the corresponding product to the cart
             cart.add(products[next])
         elif next == "DONE":
+            # The user has finished adding products, display the final cart contents
             cart.display()
+            # Thank the user for their purchases and exit the while loop
             print("Thanks for purchasing!")
             break
         else:
-            # Notify the user if a provided product code is not recognized
-            print(f"Product code {next} not recognized.")
-
-        # Display the content of the cart, including product details and prices
+            # The input was not a valid product code, inform the user
+            print(f"Product code '{next}' not recognized.")
+        
+        # Display the current contents of the cart after each product is added
         cart.display()
 
-# Ensures the main function is executed only when this script is run directly and not imported
+# This condition checks if the script is being run directly (as opposed to being imported)
+# This is good practice for making Python scripts executable as programs
 if __name__ == "__main__":
     main()
